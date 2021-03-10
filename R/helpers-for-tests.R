83cb5a3bacfb892c2377fe7f6cd11f71ef700ca7
seth127 <seth127@gmail.com>
2021-03-10 00:55:05 -0500

    test: added tests for creating new bbi_stan_model models and for the getters associated with them

--- R/helpers-for-tests.R
+++ R/helpers-for-tests.R
@@ -36,6 +36,23 @@ skip_if_not_drone_or_metworx <- function(.test_name) {
   }
 }
 
+#' Skip test if not running Stan tests
+#'
+#' First checks if `cmdstanr` is installed, then if the `SKIP_STAN_TESTS`
+#' environment variable is set.
+#' @param .test_name Character scalar to identify the test being potentially skipped.
+#'   This is printed in the skip message
+#' @keywords internal
+skip_if_no_stan <- function(.test_name) {
+  if (!requireNamespace("cmdstanr", quietly = TRUE)) {
+    testthat::skip(paste("Skipping", .test_name, "because `cmdstanr` is not installed."))
+  }
+
+  if (Sys.getenv("SKIP_STAN_TESTS") == "true") {
+    testthat::skip(paste("Skipping", .test_name, "because SKIP_STAN_TESTS set to 'true'"))
+  }
+}
+
 #' Skip test if current bbi version is below specified one.
 #'
 #' @param v a package version (or a string that can be converted to one) to pass
