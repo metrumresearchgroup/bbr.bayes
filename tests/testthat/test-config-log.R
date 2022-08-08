metrumresearchgroup/bbr

feat: made config_log() work with bbi_stan_model objects. Sets .build_data = TRUE for Stan models. Also added tests and docs.

tests/testthat/test-config-log.R
@@ -217,3 +217,26 @@ test_that("add_config() works no json found", {
   expect_equal(nrow(log_df), 1)
   expect_true(all(c(ABS_MOD_PATH, RUN_ID_COL, YAML_TAGS) %in% names(log_df)))
 })
+
+test_that("config_log() works with Stan", {
+  skip_if_no_stan("config_log() works with Stan")
+  log_df <- config_log(STAN_ABS_MODEL_DIR)
+  expect_equal(log_df[[RUN_ID_COL]], STAN_MOD_ID)
+  expect_equal(log_df[['bbi_version']], STAN_BBI_VERSION_STRING)
+  expect_false(log_df[['model_has_changed']])
+  expect_false(log_df[['data_has_changed']])
+})
+
+test_that("config_log() builds Stan data", {
+  skip_if_no_stan("config_log() builds Stan data")
+  perturb_file(
+    system.file("extdata", "fxa.data.csv", package = "bbr"),
+    content = paste(rep(99, 8), collapse = ",")
+  )
+  log_df <- config_log(STAN_ABS_MODEL_DIR)
+  expect_equal(log_df[[RUN_ID_COL]], STAN_MOD_ID)
+  expect_equal(log_df[['bbi_version']], STAN_BBI_VERSION_STRING)
+  expect_false(log_df[['model_has_changed']])
+  expect_true(log_df[['data_has_changed']])
+})
+

metrumresearchgroup/bbr.bayes

tests/testthat/test-config-log.R
@@ -268,3 +268,24 @@ test_that("config_log() works with filtering parameter string [BBR-CGLG-014]",
 
           })
 
+test_that("config_log() works with Stan", {
+  skip_if_no_stan("config_log() works with Stan")
+  log_df <- config_log(STAN_ABS_MODEL_DIR)
+  expect_equal(log_df[[RUN_ID_COL]], STAN_MOD_ID)
+  expect_equal(log_df[['bbi_version']], STAN_BBI_VERSION_STRING)
+  expect_false(log_df[['model_has_changed']])
+  expect_false(log_df[['data_has_changed']])
+})
+
+test_that("config_log() builds Stan data", {
+  skip_if_no_stan("config_log() builds Stan data")
+  perturb_file(
+    system.file("extdata", "fxa.data.csv", package = "bbr"),
+    txt = paste(rep(99, 8), collapse = ",")
+  )
+  log_df <- config_log(STAN_ABS_MODEL_DIR)
+  expect_equal(log_df[[RUN_ID_COL]], STAN_MOD_ID)
+  expect_equal(log_df[['bbi_version']], STAN_BBI_VERSION_STRING)
+  expect_false(log_df[['model_has_changed']])
+  expect_true(log_df[['data_has_changed']])
+})