f474cef140a172c38ef9d1fdb7bbdedb2c1e880e
Kyle Meyer <kylem@metrumrg.com>
2022-08-08 18:26:17 -0400

    test-print.R: drop read_bbi_path call
    It's a leftover from what the print tests in the main bbr repo need.

--- tests/testthat/test-print.R
+++ tests/testthat/test-print.R
@@ -1,17 +1,15 @@
 
 skip_if_not_drone_or_metworx("test-print")
 
-withr::with_options(list(bbr.bbi_exe_path = read_bbi_path()), {
-  withr::local_envvar(c("NO_COLOR" = "true"))
+withr::local_envvar(c("NO_COLOR" = "true"))
 
-  test_that("print.bbi_stan_model contains proper fields", {
-    fields <- c('Status',
-                'Absolute Model Path',
-                'YAML & Model Files',
-                'Tags')
+test_that("print.bbi_stan_model contains proper fields", {
+  fields <- c('Status',
+              'Absolute Model Path',
+              'YAML & Model Files',
+              'Tags')
 
-    bullets <- capture.output({ # these get thrown away, but we don't want them to print in the test output
-      purrr::walk(fields, ~ expect_message(print(STAN_MOD1), regexp = .x))
-    })
+  bullets <- capture.output({ # these get thrown away, but we don't want them to print in the test output
+    purrr::walk(fields, ~ expect_message(print(STAN_MOD1), regexp = .x))
   })
 })
