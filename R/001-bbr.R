9cbe8eb544912969e99fcf9cd795f5b4f6975ada
Kyle Meyer <kylem@metrumrg.com>
2022-08-10 14:37:27 -0400

    import generics from bbr
    These generics all have associated Stan methods.
    
    I'm placing these in a dedicated file rather than nearby the S3 method
    because it seems nice to be able to see the generics together in one
    spot.

---
+++ R/001-bbr.R
@@ -0,0 +1,11 @@
+
+# Generics
+
+#' @importFrom bbr check_up_to_date
+#' @importFrom bbr copy_model_from
+#' @importFrom bbr get_model_path
+#' @importFrom bbr get_output_dir
+#' @importFrom bbr model_diff
+#' @importFrom bbr model_summary
+#' @importFrom bbr submit_model
+NULL
