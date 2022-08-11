6373afc404fa077406bea7ed3a5210566e50f1f0
seth127 <seth127@gmail.com>
2022-08-11 16:10:48 -0400

    add helpers for opening Stan files in RStudio
    [km] These changes are being extracted from a larger commit from an
         internal repo.

---
+++ R/open.R
@@ -0,0 +1,27 @@
+
+
+##################################
+# helpers for opening bbr.bayes
+# files in Rstudio
+##################################
+
+open_stan_file <- function(.mod) {
+  checkmate::assert_class(.mod, "bbi_stan_model")  
+  .mod %>%
+    build_path_from_model(".stan") %>%
+    file.edit()
+}
+
+open_standata_file <- function(.mod) {
+  checkmate::assert_class(.mod, "bbi_stan_model")  
+  .mod %>%
+    build_path_from_model("-standata.R") %>%
+    file.edit()
+}
+
+open_init_file <- function(.mod) {
+  checkmate::assert_class(.mod, "bbi_stan_model")  
+  .mod %>%
+    build_path_from_model("-init.R") %>%
+    file.edit()
+}
