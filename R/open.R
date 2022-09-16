5990b30ad3a2d84c45675e04e6cceaba71810d58
Kyle Meyer <kylem@metrumrg.com>
2022-09-16 11:21:26 -0400

    open.R: add file.edit wrapper to silence R CMD check
    Check will complain with a plain file.edit():
    
      Undefined global functions or variables:
        file.edit
    
    However, using utils::file.edit() isn't a good option because it
    bypasses RStudio's file.edit() override, leading to different (and in
    this context undesirable) behavior: the file is opened as a popup that
    the user must save before continuing.
    
    Add a kludge to avoid the warning.

--- R/open.R
+++ R/open.R
@@ -1,27 +1,31 @@
 
-
-##################################
-# helpers for opening bbr.bayes
-# files in Rstudio
-##################################
+# Under RStudio, there is a distinction between a plain file.edit() and
+# utils::file.edit(). RStudio overrides file.edit() so that it opens up a tab.
+# utils::file.edit(), on the other hand, pops up a window that the user must
+# save before returning to RStudio.
+file_edit <- if (exists("file.edit", envir = globalenv())) {
+  get("file.edit", envir = globalenv())
+} else {
+  utils::file.edit
+}
 
 open_stan_file <- function(.mod) {
   checkmate::assert_class(.mod, "bbi_stan_model")
   .mod %>%
     build_path_from_model(".stan") %>%
-    file.edit()
+    file_edit()
 }
 
 open_standata_file <- function(.mod) {
   checkmate::assert_class(.mod, "bbi_stan_model")
   .mod %>%
     build_path_from_model("-standata.R") %>%
-    file.edit()
+    file_edit()
 }
 
 open_init_file <- function(.mod) {
   checkmate::assert_class(.mod, "bbi_stan_model")
   .mod %>%
     build_path_from_model("-init.R") %>%
-    file.edit()
+    file_edit()
 }
