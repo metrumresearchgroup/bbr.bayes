6dea6535a3cb523a3d7dcb5806f55024a32dcd8b
Kyle Meyer <kylem@metrumrg.com>
2022-09-16 11:20:11 -0400

    open.R: clean up trailing spaces

--- R/open.R
+++ R/open.R
@@ -6,21 +6,21 @@
 ##################################
 
 open_stan_file <- function(.mod) {
-  checkmate::assert_class(.mod, "bbi_stan_model")  
+  checkmate::assert_class(.mod, "bbi_stan_model")
   .mod %>%
     build_path_from_model(".stan") %>%
     file.edit()
 }
 
 open_standata_file <- function(.mod) {
-  checkmate::assert_class(.mod, "bbi_stan_model")  
+  checkmate::assert_class(.mod, "bbi_stan_model")
   .mod %>%
     build_path_from_model("-standata.R") %>%
     file.edit()
 }
 
 open_init_file <- function(.mod) {
-  checkmate::assert_class(.mod, "bbi_stan_model")  
+  checkmate::assert_class(.mod, "bbi_stan_model")
   .mod %>%
     build_path_from_model("-init.R") %>%
     file.edit()
