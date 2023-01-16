ec8dc96b3e32f2482a6d61b0f78d0242d310d610
Kyle Meyer <kylem@metrumrg.com>
2023-01-16 17:04:02 -0500

    print: add tailored method for stan_gq models
    Unlike calling print() on a bbi_stan_model, calling print() on a
    bbi_stan_gq_model shouldn't list -init.R and should list
    -fitted-params.R.

--- tests/testthat/test-print.R
+++ tests/testthat/test-print.R
@@ -7,7 +7,7 @@ test_that("print() contains proper fields", {
               'YAML & Model Files',
               'Tags')
 
-  for (mod in list(NMBAYES_MOD1, STAN_MOD1)) {
+  for (mod in list(NMBAYES_MOD1, STAN_MOD1, STAN_GQ_MOD)) {
     # Call capture.output() to prevent stdout stream from polluting test output.
     capture.output(
       purrr::walk(fields, ~ expect_message(print(mod), regexp = .x)))
@@ -16,4 +16,8 @@ test_that("print() contains proper fields", {
 
 test_that("stan: print includes -init.R", {
   expect_output(print(STAN_MOD1), regexp = "-init.R")
+})
+
+test_that("stan gq: print includes -fitted-params.R", {
+  expect_output(print(STAN_GQ_MOD), regexp = "-fitted-params.R")
 })
