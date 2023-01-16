63819f42499f1741d9e99c40b764e8d1d33941ba
Kyle Meyer <kylem@metrumrg.com>
2023-01-16 17:04:02 -0500

    test-stanargs.R: add stan_gq test cases
    The stanargs handling was updated for stan_gq models earlier in this
    series.  Add tests now that there's a stan_gq model under
    inst/model/stan/.

--- tests/testthat/test-stanargs.R
+++ tests/testthat/test-stanargs.R
@@ -1,24 +1,28 @@
 test_that("set_stanargs modifies file", {
-  tdir <- local_test_dir()
-  mod2 <- copy_model_from(STAN_MOD1, file.path(tdir, "stanmod"))
+  for (mod in list(STAN_MOD1, STAN_GQ_MOD) ) {
+    tdir <- local_test_dir()
+    mod2 <- copy_model_from(mod, file.path(tdir, "stanmod"))
 
-  file_res <- readLines(build_path_from_model(mod2, STANARGS_SUFFIX))
-  expect_false(any(grepl("parallel_chains = 8", file_res)))
+    file_res <- readLines(build_path_from_model(mod2, STANARGS_SUFFIX))
+    expect_false(any(grepl("parallel_chains = 8", file_res)))
 
-  set_stanargs(mod2, list(parallel_chains = 8))
-  file_res <- readLines(build_path_from_model(mod2, STANARGS_SUFFIX))
-  expect_true(any(grepl("parallel_chains = 8", file_res)))
+    set_stanargs(mod2, list(parallel_chains = 8))
+    file_res <- readLines(build_path_from_model(mod2, STANARGS_SUFFIX))
+    expect_true(any(grepl("parallel_chains = 8", file_res)))
+  }
 })
 
 
 test_that("set_stanargs catches reserved args", {
-  tdir <- local_test_dir()
-  mod2 <- copy_model_from(STAN_MOD1, file.path(tdir, "stanmod"))
+  for (mod in list(STAN_MOD1, STAN_GQ_MOD) ) {
+    tdir <- local_test_dir()
+    mod2 <- copy_model_from(mod, file.path(tdir, "stanmod"))
 
-  expect_error(
-    set_stanargs(mod2, list(output_dir = "naw")),
-    regexp = "Cannot add any of the following args"
-  )
+    expect_error(
+      set_stanargs(mod2, list(output_dir = "naw")),
+      regexp = "Cannot add any of the following args"
+    )
+  }
 })
 
 test_that("set_stanargs catches init", {
@@ -32,27 +36,31 @@ test_that("set_stanargs catches init", {
 })
 
 test_that("set_stanargs checks for unknown args", {
-  tdir <- local_test_dir()
-  mod2 <- copy_model_from(STAN_MOD1, file.path(tdir, "stanmod"))
+  for (mod in list(STAN_MOD1, STAN_GQ_MOD) ) {
+    tdir <- local_test_dir()
+    mod2 <- copy_model_from(mod, file.path(tdir, "stanmod"))
 
-  expect_error(
-    set_stanargs(mod2, list(youdontknowme = NULL)),
-    regexp = "not accepted"
-  )
+    expect_error(
+      set_stanargs(mod2, list(youdontknowme = NULL)),
+      regexp = "not accepted"
+    )
+  }
 })
 
 test_that("get_stanargs returns expected list", {
-  tdir <- local_test_dir()
-  mod2 <- copy_model_from(STAN_MOD1, file.path(tdir, "stanmod"))
+  for (mod in list(STAN_MOD1, STAN_GQ_MOD) ) {
+    tdir <- local_test_dir()
+    mod2 <- copy_model_from(mod, file.path(tdir, "stanmod"))
 
-  file_res <- dget(build_path_from_model(mod2, STANARGS_SUFFIX))
+    file_res <- dget(build_path_from_model(mod2, STANARGS_SUFFIX))
 
-  res <- get_stanargs(mod2)
-  expect_true(inherits(res, "bbr_stanargs"))
+    res <- get_stanargs(mod2)
+    expect_true(inherits(res, "bbr_stanargs"))
 
-  purrr::walk(names(res), ~{
-    expect_equal(res[[.x]], file_res[[.x]])
-  })
+    purrr::walk(names(res), ~{
+      expect_equal(res[[.x]], file_res[[.x]])
+    })
+  }
 })
 
 test_that("get_known_params() aborts on unknown method", {
