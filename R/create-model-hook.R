c606841fa1de1f17378314fcdc564c0c9fe2f6f3
Kyle Meyer <kylem@metrumrg.com>
2023-02-06 10:57:50 -0500

    always call check_stan_model with explicit .error value
    Spots that don't want check_stan_model() to abort rely on .error
    defaulting to TRUE.  That's fine, but pass it explicitly to make it
    easier for readers to know the intended behavior at a glance.

--- R/create-model-hook.R
+++ R/create-model-hook.R
@@ -2,6 +2,6 @@
 #' @export
 create_model_hook.bbi_stan_model <- function(.mod, ...) {
   fs::dir_create(.mod[[ABS_MOD_PATH]])
-  check_stan_model(.mod)
+  check_stan_model(.mod, .error = FALSE)
   scaffold_missing_stan_files(.mod)
 }
