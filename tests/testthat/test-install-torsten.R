c9a725f5cc7113f82624f0692d846b70f3965413
Kyle Meyer <kylem@metrumrg.com>
2023-09-18 16:41:29 -0400

    install_torsten tests: delete commented out tests

--- tests/testthat/test-install-torsten.R
+++ tests/testthat/test-install-torsten.R
@@ -23,36 +23,6 @@ test_that("install_torsten() successfully installs torsten", {
   )
 })
 
-# test_that("install_torsten() errors if it times out", {
-#   if (getRversion() < '3.5.0') {
-#     dir <- tempdir()
-#   } else {
-#     dir <- tempdir(check = TRUE)
-#   }
-#   # with quiet=TRUE
-#   expect_warning(
-#     expect_message(
-#       install_torsten(dir = dir, timeout = 1, quiet = TRUE,
-#                       release_url = torsten_test_tarball_url),
-#       "* * Installing Torsten from https://github.com",
-#       fixed = TRUE
-#     ),
-#     "increasing the value of the 'timeout' argument and running again with 'quiet=FALSE'",
-#     fixed = TRUE
-#   )
-#   # with quiet=FALSE
-#   expect_warning(
-#     expect_message(
-#       install_cmdstan(dir = dir, timeout = 1, quiet = FALSE,
-#                       release_url = cmdstan_test_tarball_url),
-#       "* * Installing Torsten from https://github.com",
-#       fixed = TRUE
-#     ),
-#     "Try increasing the value of the 'timeout' argument.",
-#     fixed = TRUE
-#   )
-# })
-
 test_that("install_torsten() errors if invalid version or URL", {
   expect_error(
     install_torsten(version = "0.89.2"),
@@ -103,35 +73,3 @@ test_that("install_torsten() works with version and release_url", {
   )
   expect_true(dir.exists(file.path(dir, "torsten_v0.89.1")))
 })
-
-# test_that("Downloads respect quiet argument", {
-#   if (getRversion() < '3.5.0') {
-#     dir <- tempdir()
-#   } else {
-#     dir <- tempdir(check = TRUE)
-#   }
-#   version <- latest_torsten_release()
-#
-# ##  ver_msg <- "trying URL 'https://api.github.com/repos/stan-dev/cmdstan/releases/latest'"
-#   download_msg <- paste0("trying URL 'https://github.com/metrumresearchgroup/Torsten/archive/refs/tags/",
-#                          version, ".tar.gz'")
-#
-#   # expect_message has trouble capturing the messages from download.file
-#   # so handle manually
-#   install_normal <- suppressWarnings(
-#     capture.output(install_torsten(dir = dir, quiet = FALSE),
-#                    type = "message")
-#   )
-#   install_quiet <- suppressWarnings(
-#     capture.output(install_torsten(dir = dir, quiet = TRUE),
-#                    type = "message")
-#   )
-#
-# ##  expect_true(any(grepl(ver_msg, install_normal, fixed = TRUE)))
-#   expect_true(any(grepl(download_msg, install_normal, fixed = TRUE)))
-#
-# ##  expect_false(any(grepl(ver_msg, install_quiet, fixed = TRUE)))
-#   expect_false(any(grepl(download_msg, install_quiet, fixed = TRUE)))
-# })
-
-
