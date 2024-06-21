a8ee5ee68c963aaeb873edc4162030eb83318241
Kyle Meyer <kylem@metrumrg.com>
2024-06-21 12:50:47 -0400

    nmbayes get_data_path: fall back to control stream path
    get_data_path.bbi_nmbayes_model() aborts if the output directory or
    init submodel doesn't exist.  That made sense when bbr's
    get_data_path() worked only with _finished_ models, pulling the path
    from bbi_config.json.  However, as of bbr v1.10.0, get_data_path()
    falls back to grabbing the data path from the control stream if
    bbi_config.json doesn't exist.
    
    Align the nmbayes method with this new behavior by dispatching to the
    parent method if the init submodel doesn't exist.

--- tests/testthat/test-get-path-from-object.R
+++ tests/testthat/test-get-path-from-object.R
@@ -41,6 +41,22 @@ test_that("nmbayes: get_data_path() parses model object", {
                                package = "bbr.bayes"))
 })
 
+test_that("nmbayes: get_data_path() falls back to control stream path", {
+  tdir <- local_test_dir()
+  model_dir <- file.path(tdir, "model", "nonmem", "bayes")
+  fs::dir_create(model_dir)
+
+  mod <- copy_model_from(NMBAYES_MOD1, file.path(model_dir, get_model_id(NMBAYES_MOD1)))
+  data_path <- file.path(tdir, "extdata", "analysis3.csv")
+
+  expect_error(get_data_path(mod), "does not exist")
+  expect_identical(get_data_path(mod, .check_exists = FALSE), data_path)
+
+  fs::dir_create(file.path(tdir, "extdata"))
+  cat("", file = data_path)
+  expect_identical(get_data_path(mod), data_path)
+})
+
 ### Stan
 
 test_that("stan: get_model_path() builds the right path", {
